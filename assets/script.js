7bf4d9df598a31c0e74f0edea542c302a6d6a63e
ZBurnell <136769106+ZBurnell@users.noreply.github.com>
2023-10-02 22:55:08 -0400

    Update script.js
    Added JS to get the main search bar working. Still need to get the 5 day forecast working

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,90 @@
-var APIKey = "5f12b4a68b9d0570bd24a3ed28d567eb";
-var city = "";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-var now = dayjs()
+var searchHistory = [];
+var lastCity = "";
+var apiKey = "5f12b4a68b9d0570bd24a3ed28d567eb";
 
 
-fetch(queryURL)
+var getCityWeather = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
-var selectedcity = UserActivation
+    fetch(apiUrl)
+         .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayWeather(data);
+                });
+            }
+        })  
+};
+
+//Search bar
+var searchSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var cityName = $("#cityname").val().trim();
+
+    if(cityName) {
+        getCityWeather(cityName);
+        $("#cityname").val("");
+        
+    } else {
+        alert("Please enter a valid city");
+    }
+};
+
+//Current Weather display
+var displayWeather = function(weatherData) {
+
+    $("#current-name").text(weatherData.name + " " + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") ).append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+    $("#current-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + "°F");
+    $("#current-humid").text("Humidity: " + weatherData.main.humidity + "%");
+    $("#current-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
+    
+
+    lastCity = weatherData.name;
+
+    saveSearchHistory(weatherData.name);
+    
+};
+
+//Save search history to local storage
+var saveSearchHistory = function (city) {
+    if(!searchHistory.includes(city)){
+        searchHistory.push(city);
+        $("#search-history").append("<a href='#' class='list-group-item list-group-item-action' id='" + city + "'>" + city + "</a>")
+    } 
+
+    localStorage.setItem("weatherSearchHistory", JSON.stringify(searchHistory));
+
+    localStorage.setItem("lastCity", JSON.stringify(lastCity));
+
+    loadSearchHistory();
+};
+
+//Load search history from local storage
+var loadSearchHistory = function() {
+    searchHistory = JSON.parse(localStorage.getItem("weatherSearchHistory"));
+    lastCity = JSON.parse(localStorage.getItem("lastCity"));
+  
+    if (!searchHistory) {
+        searchHistory = []
+    }
+    if (!lastCity) {
+        lastCity = ""
+    }
+
+    $("#search-history").empty();
+
+    for(i = 0 ; i < searchHistory.length ;i++) {
+
+       $("#search-history").append("<a href='#' class='list-group-item list-group-item-action' id='" + searchHistory[i] + "'>" + searchHistory[i] + "</a>");
+    }
+  };
+
+loadSearchHistory();
+
+//Event handlers
+$("#search-form").submit(searchSubmitHandler);
+$("#search-history").on("click", function(event){
+    var prevCity = $(event.target).closest("a").attr("id");
+    getCityWeather(prevCity);
+});
